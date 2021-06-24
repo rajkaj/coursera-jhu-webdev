7b08a7e36a2d2603ac002725466d5d183184d6a8
Raja Reddy <rajkaj123@gmail.com>
2021-06-23 22:00:16 -0400

    commit

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -2,14 +2,14 @@
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-for (var i in names) {
+  for (var i in names) {
 
-  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+    var firstLetter = ((names[i]).charAt(0)).toLowerCase();
 
-  if (firstLetter == 'j') {
-     window.byeSpeaker.speak(names[i]);
-  } else {
-     window.helloSpeaker.speak(names[i]);
+    if (firstLetter == 'j') {
+       window.byeSpeaker.speak(names[i]);
+    } else {
+       window.helloSpeaker.speak(names[i]);
+    }
   }
-}
-})(window);
+}) (window);
